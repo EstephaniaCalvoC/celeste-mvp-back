5e4a665be1bcc020438ac386bd9cc1f7203d7834
root <root@LAPTOP-A1TLA6T2.localdomain>
2023-04-23 16:22:05 -0500

    Add crawler

--- galileo-web-crawl/crawl.py
+++ galileo-web-crawl/crawl.py
@@ -1,6 +1,4 @@
-################################################################################
-### Step 1
-################################################################################
+#!/usr/bin/env python3
 
 import requests
 import re
@@ -9,175 +7,167 @@ from collections import deque
 from html.parser import HTMLParser
 from urllib.parse import urlparse
 import os
+import sys
 
-# Regex pattern to match a URL
+
 HTTP_URL_PATTERN = r'^http[s]{0,1}://(?!.*[#]).+$'
 
 
-# Define root domain to crawl
-domain = "galileo-ft.com"
-# full_url = "https://docs.galileo-ft.com/"
-
-full_url = "https://docs.galileo-ft.com/pro/docs/billpay-endpoints"
-
-         
-            
 class HyperlinkParser(HTMLParser):
+    
     def __init__(self):
         super().__init__()
-        # Create a list to store the hyperlinks
+        
         self.hyperlinks = []
-
-        # Initialize a flag to indicate whether we are inside an article tag
         self.inside_article_tag = False
+        self.skip_tag = False
 
-    # Override the HTMLParser's handle_starttag method to get the hyperlinks
     def handle_starttag(self, tag, attrs):
         attrs = dict(attrs)
 
-        # If the tag is an article tag, set the flag to True
+        if self.inside_article_tag and tag == "a" and "href" in attrs and not self.skip_tag:
+            self.hyperlinks.append(attrs["href"])
+            return
+        
         if tag == "article":
             self.inside_article_tag = True
+            return
+        
+        if tag == "section" and "class" in attrs and attrs["class"] == "Sidebar1t2G1ZJq-vU1 rm-Sidebar hub-sidebar-content":
+            self.skip_tag = True
+            return
+        
 
-        # If we are inside an article tag and the tag is an anchor tag and it has an href attribute,
-        # add the href attribute to the list of hyperlinks
-        if self.inside_article_tag and tag == "a" and "href" in attrs:
-            self.hyperlinks.append(attrs["href"])
-
-    # Override the HTMLParser's handle_endtag method to reset the flag when we exit an article tag
     def handle_endtag(self, tag):
+        if tag == "section" and self.skip_tag:
+            self.skip_tag = False
+            
         if tag == "article":
             self.inside_article_tag = False
+            
+            
 
-################################################################################
-### Step 2
-################################################################################
-
-# Function to get the hyperlinks from a URL
 def get_hyperlinks(url):
     
-    # Try to open the URL and read the HTML
     try:
-        # Open the URL and read the HTML
-        print("Open the URL and read the HTML")
-        response = requests.get(url)
-        print(url)
+        response = requests.get(url, timeout=60)
 
-        # If the response is not HTML, return an empty list
         if not response.headers['Content-Type'].startswith("text/html"):
             return []
 
-        # Decode the HTML
         html = response.content.decode('utf-8')
+
     except Exception as e:
-        print(e)
+        print(url, e)
         return []
 
-    # Create the HTML Parser and then Parse the HTML to get hyperlinks
     parser = HyperlinkParser()
     parser.feed(html)
 
     return parser.hyperlinks
 
-################################################################################
-### Step 3
-################################################################################
 
-# Function to get the hyperlinks from a URL that are within the same domain
-# TODO: It's a good idea have the seen queue as input to avoid looking form the same URL
-def get_domain_hyperlinks(local_domain, url):
-    clean_links = []
-    for link in set(get_hyperlinks(url)):
-        clean_link = None
+def get_clean_link(local_domain, link):
+    clean_link = None
 
-        # If the link is a URL, check if it is within the same domain
-        if re.search(HTTP_URL_PATTERN, link):
-            # Parse the URL and check if the domain is the same
-            url_obj = urlparse(link)
-            if url_obj.netloc == local_domain:
-                clean_link = link
+    if re.search(HTTP_URL_PATTERN, link):
+        url_obj = urlparse(link)
+        if url_obj.netloc == local_domain:
+            clean_link = link
+            
+    if link.startswith("/"):
+        clean_link = "https://" + local_domain + link
+    
+    return validate_link(clean_link) if clean_link else None
 
-        # If the link is not a URL, check if it is a relative link
-        else:
-            if link.startswith("/"):
-                link = link[1:]
-            elif (
-                link.startswith("#")
-                or link.startswith("mailto:")
-                or link.startswith("tel:")
-            ):
-                continue
-            clean_link = "https://" + local_domain + "/" + link
 
-        if clean_link is not None:
-            if clean_link.endswith("/"):
-                clean_link = clean_link[:-1]
-            clean_links.append(clean_link)
+def validate_link(clean_link):
+    is_valid = "edit" not in clean_link and "#" not in clean_link
+    return clean_link if is_valid else None
 
-    # Return the list of hyperlinks that are within the same domain
+
+def get_domain_hyperlinks(local_domain, url, level=1, clean_links=[], seen=set()):
+    if level == 0:
+        return []
+    
+    for full_link in set(get_hyperlinks(url)):
+        link = full_link.split("#")[0]
+        if link in seen:
+            continue
+        seen.add(link)
+
+        
+        clean_link = get_clean_link(local_domain, link)
+        if clean_link:
+            clean_links.append(clean_link[:-1] if clean_link.endswith("/") else clean_link)
+
+            if level > 1:
+                sub_links = get_domain_hyperlinks(local_domain, clean_link, level-1, clean_links, seen)
+                for sub_link in sub_links:
+                    if sub_link not in seen:
+                        seen.add(sub_link)
+                        clean_links.append(sub_link)
+
     return list(set(clean_links))
 
 
-################################################################################
-### Step 4
-################################################################################
-
-def crawl(url):
-    # Parse the URL and get the domain
+def crawl(url, all_levels = False, level=3):
+    
+    level = 1 if all_levels else level
+    
     local_domain = urlparse(url).netloc
 
-    # Create a queue to store the URLs to crawl
-    bill_payment_pages = get_domain_hyperlinks(local_domain, full_url)
+    bill_payment_pages = get_domain_hyperlinks(local_domain, url, level)
     bill_payment_pages.append(url)
+    
+    print(f'Processing: {len(bill_payment_pages)} pages. This could take a while.')
     
     queue = deque(bill_payment_pages)
 
-    # Create a set to store the URLs that have already been seen (no duplicates)
     seen = set(bill_payment_pages)
 
-    # Create a directory to store the text files
     if not os.path.exists("text/"):
             os.mkdir("text/")
 
     if not os.path.exists("text/"+local_domain+"/"):
             os.mkdir("text/" + local_domain + "/")
 
-    # Create a directory to store the csv files
     if not os.path.exists("processed"):
             os.mkdir("processed")
 
-    # While the queue is not empty, continue crawling
     while queue:
 
-        # Get the next URL from the queue
         url = queue.pop()
-        print(url) # for debugging and to see the progress
 
-        # Save text from the url to a <url>.txt file
         with open('text/'+local_domain+'/'+url[8:].replace("/", "_") + ".txt", "w", encoding="UTF-8") as f:
 
-            # Get the text from the URL using BeautifulSoup
             soup = BeautifulSoup(requests.get(url).text, "html.parser")
 
-            # Get the text but remove the tags
             text = soup.get_text()
 
-            # If the crawler gets to a page that requires JavaScript, it will stop the crawl
             if ("You need to enable JavaScript to run this app." in text):
                 print("Unable to parse page " + url + " due to JavaScript being required")
             
-            # Otherwise, write the text to the file in the text directory
             f.write(text)
+            
+        if all_levels:
+            for link in get_domain_hyperlinks(local_domain, url):
+                if link not in seen:
+                    queue.append(link)
+                    seen.add(link)
 
-        # Get the hyperlinks from the URL and add them to the queue
-        for link in get_domain_hyperlinks(local_domain, url):
-            if link not in seen:
-                queue.append(link)
-                seen.add(link)
-                
+
+def main():
+    if len(sys.argv) < 4:
+        print("Use mode: ./crawl.py full_ur: str all_levels: int[0, 1] level: int\n")
+        return
+    
+    full_url = sys.argv[1]
+    all_levels = int(sys.argv[2])
+    level = int(sys.argv[3])
+    
+    crawl(full_url, all_levels, level)
+    
+     
 if __name__ == "__main__":
-    #crawl(full_url)
-    local_domain = urlparse(full_url).netloc
-    bill_payment_pages = get_domain_hyperlinks(local_domain, full_url)
-    print(bill_payment_pages)
-    # get_hyperlinks("https://docs.galileo-ft.com/pro/docs/billpay-endpoints")
+    main()
